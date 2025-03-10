e7586f60ed53cc14bed6c51ed6892a5acb53fa22
NahinIntesher <nahinsikdar123@gmail.com>
2025-03-10 15:32:06 +0600

    auth page initialize

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -2,10 +2,15 @@ import ThemeSwitcher from "@/components/ThemeSwitcherButton";
 
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col ">
-      <div className="flex flex-1 items-center justify-center">
+    <div className="flex h-screen flex-col">
+      <ThemeSwitcher />
+      <div className="flex flex-1 flex-col items-center justify-center">
         Jiggasha
-        <ThemeSwitcher />
+        <div>
+          <p className="text-center">
+            Jiggasha is a platform for learning and sharing knowledge.
+          </p>
+        </div>
       </div>
     </div>
   );
