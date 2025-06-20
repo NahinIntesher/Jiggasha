fd1e90606bc2f2d59851069c9325f64adc8e4e55
NahinIntesher <nahinsikdar123@gmail.com>
2025-06-20 11:13:40 +0600

    a

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const allowedOrigins = ["http://localhost:3000", "https://jiggasha.vercel.app"];
 app.use(cookieParser());
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "https://jiggasha.vercel.app",
     credentials: true,
   })
 );
